176b8db880964e6978ee13cbb937939181daac26
Kyle Geske <stungeye@gmail.com>
2020-03-21 11:26:09 -0500

    Constrained the routes to json format only.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  resources :measured_ingredients
-  resources :measurements
-  resources :dishes
-  resources :ingredients
-  resources :restaurants
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  constraints ->(request) { request.format == :json } do
+    resources :measured_ingredients
+    resources :measurements
+    resources :dishes
+    resources :ingredients
+    resources :restaurants
+  end
 end
