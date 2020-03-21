276468db121eb86382a2f88d88a9d897818c43e3
Kyle Geske <stungeye@gmail.com>
2020-03-21 07:19:01 -0500

    Generated schema by running migration. Added schema diagram from dbdiagram.io.

---
+++ db/schema.rb
@@ -0,0 +1,57 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_03_21_120637) do
+
+  create_table "dishes", force: :cascade do |t|
+    t.string "name"
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_dishes_on_restaurant_id"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "measured_ingredients", force: :cascade do |t|
+    t.integer "dish_id", null: false
+    t.integer "ingredient_id", null: false
+    t.integer "measurement_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["dish_id"], name: "index_measured_ingredients_on_dish_id"
+    t.index ["ingredient_id"], name: "index_measured_ingredients_on_ingredient_id"
+    t.index ["measurement_id"], name: "index_measured_ingredients_on_measurement_id"
+  end
+
+  create_table "measurements", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "dishes", "restaurants"
+  add_foreign_key "measured_ingredients", "dishes"
+  add_foreign_key "measured_ingredients", "ingredients"
+  add_foreign_key "measured_ingredients", "measurements"
+end
